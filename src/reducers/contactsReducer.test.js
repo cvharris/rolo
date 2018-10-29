9b801f1d3e24246b7759a6f89ea258fb12688a0a
cvharris <charris1014@gmail.com>
2018-10-29 15:31:04 -0500

    Adds typings for tests

---
+++ src/reducers/contactsReducer.test.js
@@ -0,0 +1,21 @@
+import Contact from '../lib/Contact';
+import contactsReducer, { initialState } from './contactsReducer';
+
+it('Adds contacts to the contact list', () => {
+  const newContact = new Contact({ firstName: 'Derp' })
+
+  expect(
+    contactsReducer(initialState, {
+      type: 'ADD_CONTACT',
+      payload: newContact
+    })
+  ).toHaveLength(1)
+})
+
+it('handles unknown actions', () => {
+  expect(contactsReducer(initialState, { type: 'DERP' })).toBe(initialState)
+})
+
+it('Handles setting initial state', () => {
+  expect(contactsReducer(undefined, {})).toHaveLength(0)
+})
