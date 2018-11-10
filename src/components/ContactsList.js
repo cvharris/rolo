6cebb568c940994d9f6b98b9d21d8d3e6be5bd41
Nina Harris <ninaharris121@gmail.com>
2018-11-09 22:18:43 -0600

    Edits contact list

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import contactFields from '../lib/contactFields'
+import firebase from '../config/firebase'
 
 export default class ContactsList extends Component {
   render() {
@@ -12,7 +13,7 @@ export default class ContactsList extends Component {
     return (
       <div className="rolo-table">
         <div
-          className="rolo-table-header"
+          className="rolo-table-header pt2 tc fw6 bg-gold pb2 underline"
           style={{
             gridTemplateColumns: `repeat(${
               Object.keys(contactFields).length
@@ -28,7 +29,7 @@ export default class ContactsList extends Component {
         <div className="rolo-table-body">
           {contacts.map((contact, i) => (
             <div
-              className="contact-row items-center lh-copy pa3 ph0-l bb b--black-10"
+              className="contact-row items-center lh-copy pa3 ph0-l bb b--black-30 tc"
               key={i}
               style={{
                 gridTemplateColumns: `repeat(${
@@ -38,7 +39,11 @@ export default class ContactsList extends Component {
             >
               {Object.keys(contactFields).map((key, j) => (
                 <div className="contact-col pl3 flex-auto" key={j}>
-                  <span className="f6 db black-70">{contact[key]}</span>
+                  <span className="f6 db black-70">
+                    {contact[key] instanceof firebase.firestore.Timestamp
+                      ? contact[key].toDate().toDateString()
+                      : contact[key]}
+                  </span>
                 </div>
               ))}
             </div>
