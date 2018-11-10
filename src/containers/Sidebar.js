287ce23e3849f3c01a55fbc036bb118ce274f592
Nina Harris <ninaharris121@gmail.com>
2018-11-09 22:19:18 -0600

    Adds icons and edits sidebar

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
 class Sidebar extends Component {
   render() {
     return (
-      <header className="bg-black-90 w-100 ph3 pv3 pv4-ns ph4-m ph5-l flex items-center avenir">
-        <h1 className="f4 white mr3">Rolo</h1>
-        <nav className="f6 fw6 tracked">
+      <header className="bg-navy w-100 ph3 pv3 pv4-ns ph4-m ph5-l avenir flex justify-between items-center">
+        <h1 className="f2 white mr3 fa fa-address-book"> Rolo</h1>
+        <nav className="f3 fw6">
           <NavLink to="/">
-            <span className="link dim white dib mr3">List</span>
+            <span className="link dim white dib mr3 fa fa-list-ul"> List </span>
           </NavLink>
           <NavLink to="/new-contact">
-            <span className="link dim white dib">Add</span>
+            <span className="link dim white dib fa fa-plus"> Add</span>
           </NavLink>
         </nav>
       </header>
-    );
+    )
   }
 }
 
-export default connect((state) => ({
-}))(Sidebar);
+export default connect(state => ({}))(Sidebar)
