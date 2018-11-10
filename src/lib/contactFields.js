b62b69a1f5e52039e91b73373c15461c52fd52ea
Nina Harris <ninaharris121@gmail.com>
2018-11-09 22:20:07 -0600

    Rearranges order of contact fields

--- src/lib/contactFields.js
+++ src/lib/contactFields.js
@@ -1,10 +1,10 @@
 export default {
   firstName: 'First Name',
   middleName: 'Middle Name',
+  maidenName: 'Maiden Name',
   lastName: 'Last Name',
   phoneNumbers: 'Phone Numbers',
   email: 'Email Address',
   address: 'Home Address',
-  birthday: 'Birthday',
-  maidenName: 'Maiden Name'
+  birthday: 'Birthday'
 }
