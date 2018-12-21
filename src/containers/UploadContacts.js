890ff6b770314166b97dbc89da6fcbc650e4f1e0
cvharris <charris1014@gmail.com>
2018-12-21 15:06:53 -0600

    Adds upload instructions

--- src/containers/UploadContacts.js
+++ src/containers/UploadContacts.js
@@ -1,9 +1,9 @@
-import ContactsList from 'components/ContactsList'
-import firebase, { db } from 'config/firebase'
-import Papa from 'papaparse'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import ContactsList from 'components/ContactsList';
+import firebase, { db } from 'config/firebase';
+import Papa from 'papaparse';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 class UploadContacts extends Component {
   state = {
@@ -176,23 +176,94 @@ class UploadContacts extends Component {
     const { uploadedContacts, uploadProgress, uploading, fileSize } = this.state
 
     return (
-      <div>
+      <div className="measure-wide center pt3 pb6">
         {uploading && (
-          <div className="bg-moon-gray br-pill h1 overflow-y-hidden">
-            <div
-              className="bg-blue br-pill h1 shadow-1"
-              style={{
-                width: `${Math.round((uploadProgress / fileSize) * 100)}%`
-              }}
-            />
+          <div className="progress-bar">
+            <div className="bg-moon-gray br-pill h1 overflow-y-hidden">
+              <div
+                className="bg-blue br-pill h1 shadow-1"
+                style={{
+                  width: `${Math.round((uploadProgress / fileSize) * 100)}%`
+                }}
+              />
+            </div>
           </div>
         )}
-        <input
-          type="file"
-          name=""
-          accept=".csv"
-          onChange={e => this.handleUploadedSpreadsheet(e.target.files[0])}
-        />
+        {uploadedContacts.length === 0 && (
+          <div className="instructions lh-copy">
+            <h4>Upload Contacts</h4>
+            <p>
+              Upload a *.csv file with your contacts' information.
+              <br />
+              After uploading your contacts you will have the chance to add
+              relations and fix any bad data before your contacts are saved to
+              the database.
+            </p>
+            <input
+              type="file"
+              name=""
+              accept=".csv"
+              onChange={e => this.handleUploadedSpreadsheet(e.target.files[0])}
+            />
+            <p>All files should have the following headers:</p>
+            <ul>
+              <li>firstName</li>
+              <li>lastName</li>
+              <li>gender</li>
+              <li>birthday</li>
+              <li className="rule-header">
+                So others can use Rolo, include at least one of:
+              </li>
+              <li>phoneNumber</li>
+              <li>email</li>
+              <li className="rule-header">
+                Some of our family members are no longer with us, but you can
+                still include them in the upload, especially in order to
+                establish shared ancestry. You can include their Date of Death
+              </li>
+              <li>dod</li>
+              <li className="rule-header">
+                The following are optional but valid:
+              </li>
+              <li>homeAddressStreet1</li>
+              <li>homeAddressStreet2</li>
+              <li>homeAddressCity</li>
+              <li>homeAddressState</li>
+              <li>homeAddressZip</li>
+              <li>homeAddressCountry</li>
+              <li>maidenName</li>
+              <li>middleName</li>
+              <li>suffix</li>
+              <li>prefix</li>
+              <li className="rule-header">
+                You can automate relating contacts together by including an ID
+                for each contact and adding columns for children, parents, and
+                spouse that contain ids separated by a comma
+              </li>
+              <li>id</li>
+              <li>spouse</li>
+              <li>parents</li>
+              <li>children</li>
+            </ul>
+            <h4>How it Works</h4>
+            <p>
+              Rolo is a family tree and contact app who want to ditch sending
+              spreadsheets around and have contact information integrated with
+              their favorite contact tools automatically. You can upload a list
+              of all contact information of family members and all your family
+              members will have user accounts created for them to access the
+              same list of data.
+            </p>
+            <p>
+              For security reasons all users of Rolo can only see contacts they
+              are related to. A contact is related to another if they share a
+              common ancestor, including through marriage. To calculate this
+              correctly uploaded contacts should include relations to all
+              children, their parents, and their current spouse, if any of these
+              apply.
+            </p>
+          </div>
+        )}
         {uploadedContacts.length > 0 && (
           <div>
             <div onClick={this.uploadContacts}>Submit</div>
